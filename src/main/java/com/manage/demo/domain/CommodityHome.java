bf06a1063065d07292db8b000b78489b698dfd08
0xMilk <1144502582@qq.com>
2020-04-03 14:51:41 +0800

    init

---
+++ src/main/java/com/manage/demo/domain/CommodityHome.java
@@ -0,0 +1,88 @@
+package com.manage.demo.domain;
+
+public class CommodityHome {
+
+    String us;
+    String ye;
+    String to;
+    String ss;
+    String ys;
+    String ts;
+    String wa;
+    String se;
+    String br;
+
+    public String getUs() {
+        return us;
+    }
+
+    public void setUs(String us) {
+        this.us = us;
+    }
+
+    public String getYe() {
+        return ye;
+    }
+
+    public void setYe(String ye) {
+        this.ye = ye;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getSs() {
+        return ss;
+    }
+
+    public void setSs(String ss) {
+        this.ss = ss;
+    }
+
+    public String getYs() {
+        return ys;
+    }
+
+    public void setYs(String ys) {
+        this.ys = ys;
+    }
+
+    public String getTs() {
+        return ts;
+    }
+
+    public void setTs(String ts) {
+        this.ts = ts;
+    }
+
+    public String getWa() {
+        return wa;
+    }
+
+    public void setWa(String wa) {
+        this.wa = wa;
+    }
+
+    public String getSe() {
+        return se;
+    }
+
+    public void setSe(String se) {
+        this.se = se;
+    }
+
+    public String getBr() {
+        return br;
+    }
+
+    public void setBr(String br) {
+        this.br = br;
+    }
+
+
+}
