1c7fe334a7a22c813708c1ae17c9243c782409a1
0xMilk <1144502582@qq.com>
2020-04-04 11:14:45 +0800

    init

---
+++ src/main/java/com/manage/demo/domain/Manager.java
@@ -0,0 +1,34 @@
+package com.manage.demo.domain;
+
+public class Manager {
+    private int uuid;
+
+    private String name;
+
+    private String pwd;
+
+
+    public int getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(int uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+}
